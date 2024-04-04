044957cab7bccb1ee19be9a407978b669cc3ebd9
Locyst <154724542+Locyst@users.noreply.github.com>
2024-04-04 13:33:33 -0600

    Create amino_acid_combos.py

---
+++ amino_acid_combos.py
@@ -0,0 +1,109 @@
+amino_acid_combos = {
+    'A': {
+        'A': {
+            'A': 'Lysine',
+            'U': 'Asparagine',
+            'G': 'Lysine',
+            'C': 'Asparagine',
+        },
+        'U': {
+            'A': 'Isoleucine',
+            'U': 'Isoleucine',
+            'G': 'Methionine or Start',
+            'C': 'Isoleucine',
+        },
+        'G': {
+            'A': 'Arginine',
+            'U': 'Serine',
+            'G': 'Arginine',
+            'C': 'Serine',
+        },
+        'C': {
+            'A': 'Threonine',
+            'U': 'Threonine',
+            'G': 'Threonine',
+            'C': 'Threonine',
+        },
+    },
+
+    'U': {
+        'A': {
+            'A': 'Stop',
+            'U': 'Tyrosine',
+            'G': 'Stop',
+            'C': 'Tyrosine',
+        },
+        'U': {
+            'A': 'Leucine',
+            'U': 'Phenylalanine',
+            'G': 'Leucine',
+            'C': 'Phenylalanine',
+        },
+        'G': {
+            'A': 'Stop',
+            'U': 'Cysteine',
+            'G': 'Tryptophan',
+            'C': 'Cysteine',
+        },
+        'C': {
+            'A': 'Serine',
+            'U': 'Serine',
+            'G': 'Serine',
+            'C': 'Serine',
+        },
+    },
+
+    'G': {
+        'A': {
+            'A': 'Glutamic acid',
+            'U': 'Aspartic acid',
+            'G': 'Glutamic acid',
+            'C': 'Aspartic acid',
+        },
+        'U': {
+            'A': 'Valine',
+            'U': 'Valine',
+            'G': 'Valine',
+            'C': 'Valine',
+        },
+        'G': {
+            'A': 'Glycine',
+            'U': 'Glycine',
+            'G': 'Glycine',
+            'C': 'Glycine',
+        },
+        'C': {
+            'A': 'Alanine',
+            'U': 'Alanine',
+            'G': 'Alanine',
+            'C': 'Alanine',
+        },
+    },
+
+    "C": {
+        'A': {
+            'A': 'Glutamine',
+            'U': 'Histidine',
+            'G': 'Glutamine',
+            'C': 'Histidine',
+        },
+        'U': {
+            'A': 'Leucine',
+            'U': 'Leucine',
+            'G': 'Leucine',
+            'C': 'Leucine',
+        },
+        'G': {
+            'A': 'Arginine',
+            'U': 'Arginine',
+            'G': 'Arginine',
+            'C': 'Arginine',
+        },
+        'C': {
+            'A': 'Proline',
+            'U': 'Proline',
+            'G': 'Proline',
+            'C': 'Proline',
+        },
+    },
+}
