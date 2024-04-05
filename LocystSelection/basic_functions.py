7e193abc715e300b254157c1f89a19f50c277269
Locyst <154724542+Locyst@users.noreply.github.com>
2024-04-04 19:08:27 -0600

    Create basic_functions.py

---
+++ LocystSelection/basic_functions.py
@@ -0,0 +1,6 @@
+####################
+# Contains very basic functions that are meant to say time when you dont need something too complex
+####################
+
+def true_function(): return True
+def false_function(): return False
